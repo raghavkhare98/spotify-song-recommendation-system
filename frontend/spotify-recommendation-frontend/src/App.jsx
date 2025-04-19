d6528ad21eccb4eba89cfc0d46b9b1984932acc7
raghavkhare98 <khare08rma@gmail.com>
2025-04-18 23:56:32 -0400

    Create header

--- frontend/spotify-recommendation-frontend/src/App.jsx
+++ frontend/spotify-recommendation-frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
+import "./styles/style.css"
+import HeaderComponent from "./components/HeaderComponent";
 
 export default function App() {
   return (
     <>
+      <HeaderComponent/>
     </>
   )
 }
---
+++ frontend/spotify-recommendation-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,11 @@
+import "../styles/header.css"
+
+export default function HeaderComponent(){
+    return(
+        <div className="header">
+            <div className="logo">
+                <h1>Playlist<span>AI</span></h1>
+            </div>
+        </div>
+    )
+}
